c03c48f309ca9c1308cad68c03aac01c1cd518d5
kufie <90453759+kufiev@users.noreply.github.com>
2024-02-15 15:40:51 +0700

    Delete HospitalWeb directory

--- HospitalWeb/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$name = "hospitalweb";
-$connect = mysqli_connect($host,$user,$pass) or die ("Failed to connect to database");
-mysqli_select_db($connect,$name) or die ("Database not exist");
-?>
